893eb8bc18b6ef6e2d6c589b26095b6695c8e8a3
robeyoul-awal-raju <robeyoulawal@gmail.com>
2021-08-21 15:48:27 +0600

    total cost , net cost and coupone code finished

--- js/main.js
+++ js/main.js
@@ -1,61 +1,32 @@
-// memory
+// memory buttons
 document.getElementById('memory-8gb').addEventListener('click', function () {
   addPrice('memory-cost', 0);
   totalPrice();
-
-  //
-  /* const memorycost = document.getElementById('memory-cost');
-  const memorycostInNum = parseInt(memorycost.innerText);
-  totalCost.innerText = totalCostInNum + memorycostInNum;
-  console.log(totalCost); */
 });
 
 document.getElementById('memory-16gb').addEventListener('click', function () {
-  addPrice('memory-cost', 80);
+  addPrice('memory-cost', 180);
   totalPrice();
-
-  /*  const memorycost = document.getElementById('memory-cost');
-  const memorycostInNum = parseInt(memorycost.innerText);
-  totalCost.innerText = totalCostInNum + memorycostInNum;
-  console.log(totalCost); */
 });
 
-// storage
+// storage buttons
 
 document.getElementById('ssd-256').addEventListener('click', function () {
   addPrice('storage-cost', 0);
   totalPrice();
-
-  //
-  /*  const memorycost = document.getElementById('storage-cost');
-  const memorycostInNum = parseInt(memorycost.innerText);
-  totalCost.innerText = totalCostInNum + memorycostInNum;
-  console.log(totalCost); */
 });
 
 document.getElementById('ssd-512').addEventListener('click', function () {
-  addPrice('storage-cost', 200);
+  addPrice('storage-cost', 100);
   totalPrice();
-
-  //
-  /*  const memorycost = document.getElementById('storage-cost');
-  const memorycostInNum = parseInt(memorycost.innerText);
-  totalCost.innerText = totalCostInNum + memorycostInNum;
-  console.log(totalCost); */
 });
 
 document.getElementById('ssd-1tb').addEventListener('click', function () {
-  addPrice('storage-cost', 300);
+  addPrice('storage-cost', 180);
   totalPrice();
-
-  //
-  /* const memorycost = document.getElementById('storage-cost');
-  const memorycostInNum = parseInt(memorycost.innerText);
-  totalCost.innerText = totalCostInNum + memorycostInNum;
-  console.log(totalCost); */
 });
 
-// delivery
+// delivery buttons
 
 document.getElementById('free-delivery').addEventListener('click', function () {
   addPrice('delivery-cost', 0);
@@ -67,36 +38,47 @@ document.getElementById('paid-delivery').addEventListener('click', function () {
   totalPrice();
 });
 
-// total cost
+// promo code features
 
-// find items price
+const netBalance = document.getElementById('net-balance');
+netBalance.innerText = totalPrice();
+document.getElementById('promo-btn').addEventListener('click', function () {
+  const promoCode = document.getElementById('promo-code');
+  const promoCodeValue = promoCode.value;
+  if (promoCodeValue == 'stevekaku') {
+    netBalance.innerText = totalPrice() - totalPrice() * 0.2;
+    promoCode.value = '';
+  }
+});
+// adding price in items
+
 function addPrice(itemsName, price) {
   const Cost = document.getElementById(itemsName);
   return (Cost.innerText = price);
 }
+// individual cost
 
-function storageAmount() {
-  const itemPrice = document.getElementById('storage-cost');
+function amount(amountId) {
+  const itemPrice = document.getElementById(amountId);
   const itemPriceInNum = parseInt(itemPrice.innerText);
   return itemPriceInNum;
 }
-
-function memoryAmount() {
-  const itemPrice = document.getElementById('memory-cost');
-  const itemPriceInNum = parseInt(itemPrice.innerText);
-  return itemPriceInNum;
-}
-
-function deliveryAmount() {
-  const itemPrice = document.getElementById('delivery-cost');
-  const itemPriceInNum = parseInt(itemPrice.innerText);
-  return itemPriceInNum;
-}
+// total cost and net balance
 
 function totalPrice() {
+  const netBalance = document.getElementById('net-balance');
   const basePrice = document.getElementById('base-price');
   const basePriceInNum = parseInt(basePrice.innerText);
   const totalPrice = document.getElementById('total-cost');
+  netBalance.innerText =
+    basePriceInNum +
+    amount('storage-cost') +
+    amount('memory-cost') +
+    amount('delivery-cost');
+  console.log(netBalance);
   return (totalPrice.innerText =
-    basePriceInNum + storageAmount() + memoryAmount() + deliveryAmount());
+    basePriceInNum +
+    amount('storage-cost') +
+    amount('memory-cost') +
+    amount('delivery-cost'));
 }
